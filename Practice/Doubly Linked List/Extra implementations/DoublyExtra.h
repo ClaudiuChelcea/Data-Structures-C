59337f78b65082987ecea7930b8647a8706ccb50
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-20 11:27:46 +0200

    Add new functions

--- Practice/Doubly Linked List/Extra implementations/DoublyExtra.h
+++ Practice/Doubly Linked List/Extra implementations/DoublyExtra.h
@@ -2,47 +2,60 @@
 #define __HEADER_H_
 
 #include <errno.h>
-#define DIE(suppose,error_message) 											\
-	if(suppose) { 				   											\
-		fprintf(stderr,"At line %d in file %s!",__LINE__,__FILE__);			\
-		fprintf(stderr,"Errno value: %d with message",errno);				\
-		perror(error_message);										 		\
-		exit(errno);														\
-		}
 
-typedef struct Node
-{
-	void* data;
-	struct Node* next;
-	struct Node* prev;
-} node_t;
+#define DIE(suppose, error_message)									\
+if (suppose) {														\
+    fprintf(stderr, "At line %d in file %s!", __LINE__, __FILE__);	\
+    fprintf(stderr, "Errno value: %d with message", errno);			\
+    perror(error_message);											\
+    exit(errno);													\
+}
 
-typedef struct List
-{
-	int size;
-	int data_size;
-	struct Node* next;
-} list_t;
+typedef struct Node {
+    void * data;
+    struct Node * next;
+    struct Node * prev;
+}
+node_t;
+
+typedef struct List {
+    int size;
+    int data_size;
+    struct Node * next;
+}
+list_t;
 
 // Initialise the list
-list_t* init_list(int data_size);
+list_t * init_list(int data_size);
 
 // Print the list
-void printList(list_t* list);
+void printList(list_t * list);
 
 // Add first element
-void addFirst(list_t* list, int value);
+void addFirst(list_t ** list, int value);
 
 // Sort the list values
-void sortList(list_t* list);
+void sortList(list_t * list);
 
 // Remove the duplicates from a list
-void removeDuplicates(list_t* list);
+void removeDuplicates(list_t * list);
 
 // Inverse the list
-void reverseList(list_t* list);
+void reverseList(list_t * list);
 
 // Return the sum of the two lists
-list_t* sum_list(list_t* list1, list_t* list2);
+list_t * sum_list(list_t * list1, list_t * list2);
+
+// Merge two sorted lists
+list_t * make_merge(list_t * list1, list_t * list2);
+
+// Remove last item
+void removeLastItem(list_t ** list1);
+
+// Check if the number formed is a palindrome
+void isPalindrome(list_t * list);
+
+// Release the whole list
+void makeClean(list_t ** list);
 
 #endif
