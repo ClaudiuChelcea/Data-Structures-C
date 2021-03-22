6423810f60451b7e255fdf9625f67eb82dd46a67
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-22 11:19:50 +0200

    Add new declaration

--- Practice/Dictionaries || Hash table/functions.h
+++ Practice/Dictionaries || Hash table/functions.h
@@ -4,31 +4,35 @@
 #define NAME_SIZE 64
 
 // The hash table will store persons
-typedef struct person
-{
-	char name[NAME_SIZE];
-	int age;
-} person_t;
+typedef struct person {
+    char name[NAME_SIZE];
+    int age;
+    struct person * next;
+}
+person_t;
 
 // Create simple hash function
-unsigned int hash(const char* name);
+unsigned int hash(const char * name);
 
 // Initialise the table
-void init_table(person_t* hash_table[]);
+void init_table(person_t * hash_table[]);
 
 // Display the table
-void print_hash_table(person_t* hash_table[]);
+void print_hash_table(person_t * hash_table[]);
 
 // Insert person to the table
-void insert_person(person_t* person, person_t* hash_table[]);
+void insert_person(person_t * person, person_t * hash_table[]);
 
 // Return one person by name or NULL
-person_t* hash_table_find(const char* name, person_t* hash_table[]);
+person_t * hash_table_find(const char * name, person_t * hash_table[]);
 
 // Display a single person
-void printPerson(person_t* person);
+void printPerson(person_t * person);
 
 // 	Remove person by name
-void deletePerson(const char* name, person_t* hash_table[]);
+int deletePerson(const char * name, person_t * hash_table[]);
+
+// Clean the whole list
+void delete_hash_table(person_t * hash_table[]);
 
 #endif
